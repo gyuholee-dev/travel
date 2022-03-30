9600ca0087686781f5f8af84fba29c4a91433279
LeeGyuho <andrewgyuholee@gmail.com>
2022-03-30 12:32:36 +0900

    백엔드 기초기능 구현 시작

---
+++ includes/init.php
@@ -0,0 +1,14 @@
+<?php // init.php
+
+// 글로벌 변수
+global $DB;
+global $PAGE;
+global $TITLE;
+
+// DB 환경변수
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$database = 'travel'; // TODO: 데이터베이스 이름 결정 요망
+$DB = mysqli_connect($host, $user, $pass);
+mysqli_select_db($DB, $database);
