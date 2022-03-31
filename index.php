d0d1ca07b38cb35a53f876c55c876eee6c5acbc8
noel1967 <noel1967@naver.com>
2022-03-31 11:16:02 +0900

    index만 올림

---
+++ index.php
@@ -0,0 +1,218 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ShinWonJeong</title>
+        <meta name="viewport" content="
+        width=device-width, initial-scale=1.0">
+        <link rel="icon" href="images/logo/logo5.png">
+        <link rel="stylesheet" href="styles/style.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="javascript/script.js" defer="defer"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mrs+Saint+Delafield&family=Nanum+Pen+Script&display=swap" rel="stylesheet">
+     </head>
+    <body>
+        <header>
+        <div id="top">
+          <div class="logo">
+            <a href="index.php"><img src="images/logo/logo2.png"></a>
+          </div>
+          <div class="dropwhere">
+            <div class="dropdown-btn"><p>어디로 가세요?▽</p></div>
+            <div class="dropwhere-cont">
+              <p><a href="#">국내여행지</a></p>
+              <p><a href="#">해외여행지</a></p>
+              <p><a href="#">추천투어</a></p>
+              <p><a href="#">블로그</a></p>
+              <p><a href="#">커뮤니티</a></p>
+            </div><!-- dropwhere-cont -->
+          </div><!-- dropwhere -->
+          <div class="right">
+            <P><a href="#">회사소개</a></P>
+            <P><a href="#">공지사항</a></P>
+            <P><a href="#">내여행</a></P>
+            <P><a href="#">로그인</a></P>
+          </div><!-- right -->
+        </div><!-- top -->
+        </header>
+        <section id="content">   
+          <div class="search">
+            <div class="search-box">
+            <div class="search-top">
+              <p><a href="#">국내투어</a></p>
+              <p><a href="#">해외투어</a></p>
+            </div><!-- search-top -->
+            <div class="search-bottom">
+              <form name="frm1" method="post" action="search_ok.php">
+                <br><br>
+                <table class="table1">
+                    <tr>
+                       <td><input type="text" name="place" value="목적지를 검색하세요"></td>
+                       <td><input type="button" value="search" class="bt" onclick="send()"></td>
+                    </tr>
+                </table><br><br>   
+          </form>
+            </div><!-- search-bottom -->
+          </div><!-- search-box -->
+          </div><!-- search -->
+          <div class="main-event">
+            <div class="main-event-img"><img src=""></div>
+            <div class="main-event-write">
+              <p>리워드를 통한 확인</p>
+              <p>블라썸투어의 야침한 이벤트 여기서 확인하세요</p>
+            </div>
+            <div class="main-event-button">
+              <input type="button" value="search" class="bt" onclick="location.href='#'">
+            </div>
+
+          </div><!-- main-event -->
+          <div class="helper">여행 도우미</div><!-- helper -->
+          <div class="helper-cont">
+            <div><a href="#">
+              <img src="">
+              <p>FAQ</p></a>
+            </div>
+            <div><a href="#">
+              <img src="">
+              <p>1:1 고객센터</p></a>
+            </div>
+            <div><a href="#">
+              <img src="">
+              <p>확인/변경</p></a>
+            </div>
+          </div><!-- helper-cont -->
+          <div class="promotion">
+            <p>이달의 프로모션</p>
+          </div><!-- promotion -->
+          <div class="promotion-cont">
+            <div class="promo-left">
+              <a href="#">
+                <img src="images/">
+                <p>설명내용추가</p>
+              </a>
+            </div>
+            <div class="promo-right">
+              <a href="#">
+                <img src="images/">
+                <div>
+                <p>설명내용추가</p>
+                <p>타이틀</p>
+                <p>설명내용추가</p>
+                </div>
+              </a>
+            </div>
+          </div><!-- promotion-cont -->
+          <div class="theme">블라썸 투어의 테마여행</div><!-- theme -->
+          <div class="theme-cont">
+            <div class="theme-icons">
+              <div class="t-icon">
+                <img src="images/">
+                <P>역사·문화</P>
+              </div>
+              <div class="t-icon">
+                <img src="images/">
+                <P>시티라이프</P>
+              </div>
+              <div class="t-icon">
+                <img src="images/">
+                <P>산꼴라이프</P>
+              </div>
+            </div><!-- theme-icons -->
+            <div class="theme-imgs1">
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+            </div><!-- theme-imgs1 -->
+            <div class="theme-imgs2">
+              <div class="t-images">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+            </div><!-- theme-imgs2 -->
+            <div class="theme-imgs3">
+              <div class="t-images">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+              <div class="t-images"><a href="#">
+                <img src="images/">
+                <p>여기에 타이틀 내용적어주세요</p>
+                <p>500,000원 부터~</p>
+                <p class="hash">자연</p>
+                <p class="hash">경치</p>
+                <p class="hash">나무</p>
+              </a></div>
+            </div><!-- theme-imgs3 -->
+          </div><!-- theme-cont -->
+        </section>
+        <div class="clear"></div>
+        <footer>
+          <div class="bottom-logo"><a href="index.php">
+            <img src="images/logo/logo4.png"></a>
+          </div>
+          <div class="policy">
+            <p></p>
+          </div>
+          <div class="c-info">
+            <P></P>
+          </div>
+          <div class="social">
+            <a href=""></a>
+          </div>
+        </footer>
+   </body>
+</html>
