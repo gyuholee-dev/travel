0b5bc8794753bd7e360273c0ba217eb3ee0cef35
LeeGyuho <andrewgyuholee@gmail.com>
2022-03-31 12:33:31 +0900

    템플릿 기초기능 추가

--- includes/init.php
+++ includes/init.php
@@ -1,12 +1,26 @@
 <?php // init.php
 
-// 글로벌 변수
+// 사이트 정보
+global $INFO;
+
+// 환경변수
 global $DB;
 global $PAGE;
-global $TITLE;
+global $ACT;
+global $CODE;
+global $DO;
 
-// DB 환경변수
-if ($_SERVER['HTTP_HOST']=='localhost') {
+
+// 사이트 정보
+$INFO = [
+  'title'=>'블라썸투어',
+  'subtitle'=>'여행을 즐기는 방법',
+  'description'=>'블라썸투어는 여행을 즐기는 방법을 소개합니다.',
+  'author'=>'블라썸투어',
+];
+
+// DB
+/* if ($_SERVER['HTTP_HOST']=='localhost') {
   // 서버가 로컬호스트일 경우 기본 DB 설정파일을 불러옴
   $dbConfig = json_decode(file_get_contents('configs/db.localhost.json'),true);
 } else {
@@ -17,4 +31,18 @@ $host = $dbConfig['host'];
 $user = $dbConfig['user'];
 $pass = $dbConfig['pass'];
 $database = $dbConfig['database'];
-$DB = mysqli_connect($host, $user, $pass, $database);
+$DB = mysqli_connect($host, $user, $pass, $database); */
+
+/* 리퀘스트
+page: main, about, tour, contact, etc...
+action 액션: list, item...
+code: 상품코드
+do 실행: 
+  item&do=view
+          edit
+          delete
+*/
+$PAGE = isset($_REQUEST['page'])?$_REQUEST['page']:'top';
+$ACT = isset($_REQUEST['action'])?$_REQUEST['action']:'list';
+$DO = isset($_REQUEST['action'])?$_REQUEST['action']:'view';
+$CODE = isset($_REQUEST['code'])?$_REQUEST['code']:'00000000';
--- main.php
+++ main.php
@@ -5,3 +5,80 @@ require_once 'includes/init.php';
 // 함수
 require_once 'includes/functions.php';
 
+// 컨텐츠
+$head = '';
+$header = '';
+$nav = '';
+$content = '';
+$aside = '';
+$footer = '';
+
+// 헤드
+$head = <<<HTML
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="favicon.ico">
+  <title>$INFO[title] : $INFO[subtitle]</title>
+  <meta name="description" content="$INFO[description]">
+  <link rel="stylesheet" href="styles/style.css">
+  <script type="text/javascript" src="scripts/script.js"></script>
+HTML;
+
+$header = <<<HTML
+  <div id="top">
+    <div class="logo">
+      <a href="index.php"><img src="images/logo/logo2.png"></a>
+    </div>
+    <div class="dropwhere">
+      <div class="dropdown-btn"><p>어디로 가세요?▽</p></div>
+      <div class="dropwhere-cont">
+        <p><a href="#">국내여행지</a></p>
+        <p><a href="#">해외여행지</a></p>
+        <p><a href="#">추천투어</a></p>
+        <p><a href="#">블로그</a></p>
+        <p><a href="#">커뮤니티</a></p>
+      </div>
+    </div>
+    <div class="right">
+      <P><a href="#">회사소개</a></P>
+      <P><a href="#">공지사항</a></P>
+      <P><a href="#">내여행</a></P>
+      <P><a href="#">로그인</a></P>
+    </div>
+  </div>
+HTML;
+
+// 푸터
+$footer = <<<HTML
+  <div class="bottom-logo">
+    <a href="index.php"><img src="images/logo/logo4.png"></a>
+  </div>
+  <div class="policy">
+    <p></p>
+  </div>
+  <div class="c-info">
+    <P></P>
+  </div>
+  <div class="social">
+    <a href=""></a>
+  </div>
+HTML;
+
+// 콘텐츠
+include "pages/$PAGE.php";
+
+// 랜더링 --------------------------------------------------
+
+$content_values = array( 
+  '{head}' => $head,
+  '{header}' => $header,
+  '{nav}' => $nav,
+  '{content}' => $content,
+  '{aside}' => $aside,
+  '{footer}' => $footer,
+);
+
+$html = file_get_contents('templates/template.html');
+$html = strtr($html, $content_values);
+echo $html;
