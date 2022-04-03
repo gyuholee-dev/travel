bd29c98a47cc7a33607c98956823df5723f4421d
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-04 05:51:08 +0900

    place 페이지의 상품리스트를 템플릿으로 전환

--- pages/place.php
+++ pages/place.php
@@ -3,6 +3,9 @@
 // 리퀘스트
 $search = $_REQUEST['search'] ?? '';
 $category = $_REQUEST['category'] ?? '전체'; // 국내, 해외, 전체
+if ($category != '전체' && $category != '국내' && $category != '해외') {
+  $category = '전체';
+}
 
 // 서치박스 --------------------------------------------------
 
@@ -22,12 +25,20 @@ $searchBox_data = [
 ];
 $searchBox = renderElement(TPL.'searchBox.html', $searchBox_data);
 
-// 랜더링 --------------------------------------------------
+// 상품리스트 --------------------------------------------------
 
+// 프로덕트리스트
+$productList = getProductList($category, $search);
 
+$productBox_data = [
+  'title' => $category=='전체'?'전체 상품':$category.'투어 모든상품',
+  'productList' => $productList
+];
+$productBox = renderElement(TPL.'productBox.html', $productBox_data);
 
 // 랜더링 --------------------------------------------------
 $content_data = [
   'searchBox' => $searchBox,
+  'productBox' => $productBox,
 ];
 $content .= renderElement(HTML.'place.html', $content_data);
