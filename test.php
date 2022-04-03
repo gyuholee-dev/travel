f99dfc2db9a7b01236f01d346d9f24f051635acc
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-04 06:21:07 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,15 @@
 <?php
-echo '32자 임의 코드'.'<br>';
-echo md5('dedawdwwddawdw').'<br>';
-echo md5('dwdwwdw').'<br>';
-echo md5(time()).'<br>';
+// echo '32자 임의 코드'.'<br>';
+// echo md5('dedawdwwddawdw').'<br>';
+// echo md5('dwdwwdw').'<br>';
+// echo md5(time()).'<br>';
 
 // echo mb_strlen('자연,경치,나무,김치,경치');
-echo date("Y-m-d H:i:s", time());
+// echo date("Y-m-d H:i:s", time());
+// echo '<br>';
+
+// print_r($_SERVER);
+
+// echo basename(__FILE__);
+// echo basename(__DIR__);
+// basename(getcwd());
