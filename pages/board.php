ca2fdc4af9064d6e742fee214464a0b3c3ec43c2
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-04 14:46:21 +0900

    메인메뉴 링크 추가, 게시판 및 멤버 페이지 추가.

---
+++ pages/board.php
@@ -0,0 +1,7 @@
+<?php // board.php
+
+// category: notice(공지사항), faq(자주묻는질문), qna(질문과답변), free(자유게시판)
+
+// 랜더링 --------------------------------------------------
+$content_data = [];
+$content .= renderElement(HTML.'top.html', $content_data);
