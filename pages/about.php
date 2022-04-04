0e752a185e35bcdf00e755257a72733b3abcc64b
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-04 15:03:55 +0900

    어바웃 페이지 수정

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,11 @@
 <?php // about.php
 
+$siteTitle = $INFO['title'];
+$siteDesc = $INFO['description'];
 
 // 랜더링 --------------------------------------------------
-$content_data = [];
+$content_data = [
+  'siteTitle' => $siteTitle,
+  'siteDesc' => $siteDesc,
+];
 $content .= renderElement(HTML.'about.html', $content_data);
