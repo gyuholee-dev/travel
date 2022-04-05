1a427c5813344398767d085f5a04138d80bc2c09
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-05 16:34:17 +0900

    마이페이지 추가, 헤더 링크 수정

---
+++ pages/mypage.php
@@ -0,0 +1,9 @@
+<?php // mypage.php
+/* 
+action=home
+ */
+
+
+// 랜더링 --------------------------------------------------
+$content_data = [];
+$content .= renderElement(HTML.'mypage.html', $content_data);
